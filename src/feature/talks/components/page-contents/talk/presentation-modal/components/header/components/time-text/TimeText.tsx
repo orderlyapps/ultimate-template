49f4a96dc24842895bb18066d30967512b9c7dc0
orderlyapps <orderlyapps@gmail.com>
2026-01-07 06:00:05 +1100

    refactor: extract formatMinutesSeconds helper function from TimeText component.

--- src/feature/talks/components/page-contents/talk/presentation-modal/components/header/components/time-text/TimeText.tsx
+++ src/feature/talks/components/page-contents/talk/presentation-modal/components/header/components/time-text/TimeText.tsx
@@ -1,13 +1,7 @@
 import { Text } from "@ionic-display/text/Text";
 import { useCurrentTalkPresentationCountdown } from "../../../../hooks/use-current-talk-presentation-countdown/useCurrentTalkPresentationCountdown";
 import { IonTitle } from "@ionic/react";
-
-function formatMinutesSeconds(totalSeconds: number) {
-  const safeSeconds = Math.max(0, Math.floor(totalSeconds));
-  const minutes = Math.floor(safeSeconds / 60);
-  const seconds = safeSeconds % 60;
-  return `${minutes}:${String(seconds).padStart(2, "0")}`;
-}
+import { formatMinutesSeconds } from "./helper/formatMinutesSeconds";
 
 type Props = {
   remainingSeconds?: number;
