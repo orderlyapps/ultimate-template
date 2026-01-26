7309c1defafef2e187e1ed74ea2c0570541ad528
orderlyapps <orderlyapps@gmail.com>
2026-01-26 21:37:14 +1100

    restyle unit modal

--- src/feature/maps/door-to-door/components/not-at-homes/components/not-at-home-unit-modal/NotAtHomeUnitModal.tsx
+++ src/feature/maps/door-to-door/components/not-at-homes/components/not-at-home-unit-modal/NotAtHomeUnitModal.tsx
@@ -1,10 +1,26 @@
-import { IonModal, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton, IonButtons } from "@ionic/react";
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonListHeader,
+  IonNote,
+} from "@ionic/react";
 import { useDoorToDoorStore } from "@feature/maps/door-to-door/store/useDoorToDoorStore";
 import { formatDistanceToNow } from "date-fns";
+import { List } from "@ionic-layout/list/List";
+import { Item } from "@ionic-layout/item/Item";
+import { Label } from "@ionic-display/label/Label";
+import { Space } from "@layout/space/Space";
+import { CloseButton } from "@input/button/close-button/CloseButton";
 
 export const NotAtHomeUnitModal: React.FC = () => {
   const selectedUnits = useDoorToDoorStore((state) => state.selectedUnits);
-  const setSelectedUnits = useDoorToDoorStore((state) => state.setSelectedUnits);
+  const setSelectedUnits = useDoorToDoorStore(
+    (state) => state.setSelectedUnits,
+  );
 
   const isOpen = !!selectedUnits && selectedUnits.length > 0;
 
@@ -17,34 +33,61 @@ export const NotAtHomeUnitModal: React.FC = () => {
   const firstUnit = selectedUnits[0];
   const address = `${firstUnit.house_number} ${firstUnit.street}, ${firstUnit.suburb}`;
 
+  const writeUnits = selectedUnits.filter((unit) => unit.write);
+  const returnUnits = selectedUnits.filter((unit) => !unit.write);
+
   return (
     <IonModal isOpen={isOpen} onDidDismiss={handleDismiss}>
       <IonHeader>
         <IonToolbar>
           <IonTitle>{address}</IonTitle>
           <IonButtons slot="end">
-            <IonButton onClick={handleDismiss}>Close</IonButton>
+            <CloseButton onClick={handleDismiss}></CloseButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonList>
-          {selectedUnits.map((unit) => {
-            const created = unit.created_at
-              ? formatDistanceToNow(new Date(unit.created_at), { addSuffix: true })
-              : "Unknown";
+        {returnUnits.length > 0 && (
+          <List>
+            <IonListHeader>Return List</IonListHeader>
+            {returnUnits.map((unit) => {
+              const created = unit.created_at
+                ? formatDistanceToNow(new Date(unit.created_at), {
+                    addSuffix: true,
+                  })
+                : "Unknown";
 
-            return (
-              <IonItem key={unit.id}>
-                <IonLabel>
-                  <h2>Unit {unit.unit_number}</h2>
-                  <p>Added {created}</p>
-                  <p>{unit.write ? "Write" : "Return"}</p>
-                </IonLabel>
-              </IonItem>
-            );
-          })}
-        </IonList>
+              return (
+                <Item key={unit.id} className="ion-margin">
+                  <Label>Unit {unit.unit_number || "N/A"}</Label>
+                  <IonNote>Added {created}</IonNote>
+                </Item>
+              );
+            })}
+          </List>
+        )}
+
+        <Space height="2" />
+
+        {writeUnits.length > 0 && (
+          <List>
+            <IonListHeader>Write List</IonListHeader>
+            {writeUnits.map((unit) => {
+              const created = unit.created_at
+                ? formatDistanceToNow(new Date(unit.created_at), {
+                    addSuffix: true,
+                  })
+                : "Unknown";
+
+              return (
+                <Item key={unit.id} className="ion-margin">
+                  <Label>Unit {unit.unit_number || "N/A"}</Label>
+                  <IonNote>Added {created}</IonNote>
+                </Item>
+              );
+            })}
+          </List>
+        )}
       </IonContent>
     </IonModal>
   );
