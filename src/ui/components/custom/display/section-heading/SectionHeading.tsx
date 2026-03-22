6d13388cb4994976ba09b01d5827ff4aa4dbd8c3
orderlyapps <orderlyapps@gmail.com>
2026-03-22 15:50:03 +1100

    add SectionHeading component with primary color & xl size styling, remove .gitkeep from display folder

---
+++ src/ui/components/custom/display/section-heading/SectionHeading.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@ionic-display/text/Text";
+
+interface SectionHeadingProps {
+  children: string;
+}
+
+export const SectionHeading: React.FC<SectionHeadingProps> = ({ children }) => {
+  return (
+    <Text color="primary" size="xl" className="ion-margin">
+      {children}
+    </Text>
+  );
+};
