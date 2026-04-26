9dbdfbe29c821c2fc8db44a74e1091863883677f
orderlyapps <orderlyapps@gmail.com>
2026-04-26 19:22:00 +1000

    move import/export buttons from PublisherListsHeaderButtons to Publishers page header, remove feature access check and unused imports

--- src/content/publishers/lists/components/publisher-lists-header-buttons/PublisherListsHeaderButtons.tsx
+++ src/content/publishers/lists/components/publisher-lists-header-buttons/PublisherListsHeaderButtons.tsx
@@ -1,39 +1,8 @@
 import { AddButton } from "@input/button/add-button/AddButton";
-import { FileExport } from "@input/file-export/FileExport";
-import { FileImport } from "@input/file-import/FileImport";
-import {
-  exportPublisherLocalData,
-  generateExportFilename,
-} from "@state/tanstack/db/publisher-local/publisherLocalExportImport";
-import {
-  useFeatureAccess,
-  TEMP_ALL_AUTHORIZED_NAMES,
-} from "@services/app/auth/temp-feature-access/useFeatureAccess";
 import { usePublisherListsStore } from "../../store/usePublisherListsStore";
 
 export const PublisherListsHeaderButtons: React.FC = () => {
-  const { setIsAddModalOpen, setPendingImportFile } = usePublisherListsStore();
+  const { setIsAddModalOpen } = usePublisherListsStore();
 
-  const { isUnlocked } = useFeatureAccess(TEMP_ALL_AUTHORIZED_NAMES);
-
-  return (
-    <>
-      {isUnlocked && (
-        <>
-          <FileImport
-            onFileSelect={(file) => setPendingImportFile(file)}
-            accept=".json"
-            iconOnly
-          />
-          <FileExport
-            getData={exportPublisherLocalData}
-            filename={generateExportFilename()}
-            iconOnly
-          />
-
-          <AddButton onClick={() => setIsAddModalOpen(true)} />
-        </>
-      )}
-    </>
-  );
+  return <AddButton onClick={() => setIsAddModalOpen(true)} />;
 };
--- src/routes/pages/publishers/Publishers.tsx
+++ src/routes/pages/publishers/Publishers.tsx
@@ -4,15 +4,37 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonButtons,
 } from "@ionic/react";
 import { PublishersContent } from "@/content/publishers/PublishersContent";
+import { FileExport } from "@input/file-export/FileExport";
+import { FileImport } from "@input/file-import/FileImport";
+import {
+  exportPublisherLocalData,
+  generateExportFilename,
+} from "@state/tanstack/db/publisher-local/publisherLocalExportImport";
+import { usePublisherListsStore } from "@/content/publishers/lists/store/usePublisherListsStore";
 
 export const Publishers: React.FC = () => {
+  const { setPendingImportFile } = usePublisherListsStore();
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Publishers</IonTitle>
+          <IonButtons slot="end">
+            <FileImport
+              onFileSelect={(file) => setPendingImportFile(file)}
+              accept=".json"
+              iconOnly
+            />
+            <FileExport
+              getData={exportPublisherLocalData}
+              filename={generateExportFilename()}
+              iconOnly
+            />
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding">
